60500b3956133248e1e5a68a373ca6143c2282c7
Aitor <aitormartinezcedillo@gmail.com>
2025-01-02 17:53:20 +0100

    Pruebas realizadas

--- src/test/java/com/curso/controller/test/TestHotelController.java
+++ src/test/java/com/curso/controller/test/TestHotelController.java
@@ -1,10 +1,13 @@
 package com.curso.controller.test;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Arrays;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,9 +27,13 @@ class TestHotelController {
 	
 	@BeforeEach
 	void setUp() {
-		Hotel hotel = new Hotel ();
+		Hotel hotel = new Hotel (1, "hotelBonito", "Playa", 99.99d, true);
 		when(service.findById(1)).thenReturn(hotel);
 		when(service.hoteles()).thenReturn(Arrays.asList(hotel));
 	}
-
+	@Test
+	void testGetHotel() throws Exception{
+		mockMvc.perform(get("/api/hoteles"))
+			.andExpect(status().isOk());
+	}
 }
